1391acebff813fbbeb078bde24ea8b543ed34a2d
Felipe Mattedi <felipe.mattedi@terra.com.br>
2021-05-02 12:29:09 -0300

    Adiciona função de postagem

--- src/Escrever.jsx
+++ src/Escrever.jsx
@@ -1,17 +1,50 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 function Escrever() {
+
+  const [texto, alteratexto] = useState('')
+
+  const modificatexto = (evento) => {
+    if(evento.target.value.length <= 300){
+    alteratexto(evento.target.value)}
+  }
+
+   async function postamensagem() {
+
+    var d = new Date();
+    var months = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
+    var data = d.getDate() + ' ' + months[d.getMonth()] + ' ' + d.getFullYear()
+
+    var post = [
+      { 
+        "usuario" : "felipenick",
+        "post": texto,
+        "data": data
+      }
+    ]
+  
+    var metodo = { 
+                    method: 'PUT',
+                    body: post
+                   }
+
+    const resultado = await fetch(`https://j0rjodfah4.execute-api.us-east-1.amazonaws.com/backend-redesocial-lc`, metodo)
+  
+  }
+
   return (
     <div className='postagem'>
       <Form>
         <Form.Group controlId="exampleForm.ControlTextarea1">
           <Form.Label>O que você está pensando?</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control as="textarea" rows={3} onChange={modificatexto} value={texto} />
         </Form.Group>
       </Form>
+      Caracteres restantes {300 - texto.length}
       <div className='botao'>
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" onClick={postamensagem}>
       Postar
       </Button>
       </div>
